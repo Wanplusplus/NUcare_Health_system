33fc9df3cc7334fda86b043c38399c52e5b8e7e9
G1yck4 <glyckamabag@gmail.com>
2026-05-21 18:36:05 +0800

    ignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nucare/health-system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2cea0f0d20d412d11d27053cb2fe8c27bb6cee50',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nucare/health-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2cea0f0d20d412d11d27053cb2fe8c27bb6cee50',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.1.0',
+            'version' => '7.1.0.0',
+            'reference' => 'e193b921a73e65bff40cab89ea0b0293be0e0c20',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
